8c555ca3b21a3017b5eb0efb2c3cd9a6d66a54e2
Mohamed Kouissi <mohamed.kouissi@gmail.com>
2025-05-07 13:43:25 +0100

    My first MVI archi (add screens, intent, viewModel, repository): WIP

---
+++ app/src/main/java/com/example/emtyapp/ui/product/component/ProductItemComponent.kt
@@ -0,0 +1,35 @@
+package com.example.emtyapp.ui.product.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+import com.example.emtyapp.data.Entities.Product
+
+@Composable
+fun ProductItem(product: Product, onNavigateToDetails: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+            .shadow(4.dp, RoundedCornerShape(8.dp)),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(text = "Title: ${product.title}")
+            Text(text = "Description: ${product.description}")
+            Button(onClick = { onNavigateToDetails(product.productId) }) {
+                Text(text = "Plus de détails...")
+            }
+        }
+    }
+}
